dab5c009d0c4d887a0e9fba63139a3a1a18554f2
Julian Edington <julian.edington@outlook.com>
2021-09-08 18:02:05 -0500

    WIP, further tweaks on scripts/instructions

--- 00-Setup-Ansible (optional)/kubernetes-ubuntu-setup.sh
+++ 00-Setup-Ansible (optional)/kubernetes-ubuntu-setup.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
-# store in /home/<user>/documents of the controller
+#!/bin/bash
+# store in controller host '~/<this file>'
 
 sudo apt-get install -y containerd
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-apt-get update
-apt-get install -y kubelet kubeadm kubectl
+apt update
+apt install -y kubelet kubeadm kubectl
 apt-mark hold kubelet kubeadm kubectl containerd
 
 #etc.
---
+++ 00-Setup-Ansible (optional)/ssh-setup-example.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# store in controller host '~/<this file>'
+# this is optional
+# example commands to setup ssh--might need to change/add root@'s
+
+ssh-keygen -t rsa -b 4096 -C "Ansible Key"
+ssh-copy-id -i $HOME/.ssh/id_rsa.pub root@localhost
+ssh-copy-id -i $HOME/.ssh/id_rsa.pub root@c1.node1
+ssh-copy-id -i $HOME/.ssh/id_rsa.pub root@c1.node2
+ssh-copy-id -i $HOME/.ssh/id_rsa.pub root@c1.node3
+eval $(ssh-agent)
+ssh-add
+ansible -i ~/hosts -m ping all
+
+#- sudo ansible-playbook -i ~/ all-setup.yml
+# ^ run after setting up ssh/other auth options ^
