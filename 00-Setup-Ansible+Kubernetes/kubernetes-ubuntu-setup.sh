5b29dac3fd181723f8acade1e83b008b93f52983
Julian Edington <julian.edington@outlook.com>
2021-09-08 10:30:14 -0500

    rolling back to a simpler script

--- 00-Setup-Ansible+Kubernetes/kubernetes-ubuntu-setup.sh
+++ 00-Setup-Ansible+Kubernetes/kubernetes-ubuntu-setup.sh
@@ -1,46 +1,6 @@
 #!/bin/sh
 # store in /home/<user>/documents of the controller
 
-set -euo pipefail # fail on error , debug all lines
-
-info() {
-  printf "\r\033[00;35m$1\033[0m\n"
-}
-
-success() {
-  printf "\r\033[00;32m$1\033[0m\n"
-}
-
-fail() {
-  printf "\r\033[0;31m$1\033[0m\n"
-}
-
-divider() {
-  printf "\r\033[0;1m========================================================================\033[0m\n"
-}
-
-pause_for_confirmation() {
-  read -rsp $'Press any key to continue (ctrl-c to quit):\n' -n1 key
-}
-
-# Set up an interrupt handler so we can exit gracefully
-interrupt_count=0
-interrupt_handler() {
-  ((interrupt_count += 1))
-
-  echo ""
-  if [[ $interrupt_count -eq 1 ]]; then
-    fail "Really quit? Hit ctrl-c again to confirm."
-  else
-    echo "Goodbye!"
-    exit
-  fi
-}
-trap interrupt_handler SIGINT SIGTERM
-
-sudo -n true
-test $? -eq 0 || exit 1 "sudo priviledge Required to run script"
-
 sudo apt-get install -y containerd
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
