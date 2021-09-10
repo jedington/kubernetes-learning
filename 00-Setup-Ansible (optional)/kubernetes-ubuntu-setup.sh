0f795d9dc9b5a58082195a48d223b223bda9ff66
Julian Edington <julian.edington@outlook.com>
2021-09-10 06:05:48 -0500

    mostly automated option toward setting up nodes

--- 00-Setup-Ansible (optional)/kubernetes-ubuntu-setup.sh
+++ 00-Setup-Ansible (optional)/kubernetes-ubuntu-setup.sh
@@ -1,13 +1,71 @@
 #!/bin/bash
 # store in controller host '~/<this file>'
 
-sudo apt-get install -y containerd
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb https://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-apt update
-apt install -y kubelet kubeadm kubectl
-apt-mark hold kubelet kubeadm kubectl containerd
+#Install a container runtime - containerd
+#containerd prerequisites, first load two modules and configure them to load on boot
+sudo modprobe overlay
+sudo modprobe br_netfilter
 
-#etc.
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+#Setup required sysctl params, these persist across reboots.
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+#Apply sysctl params without reboot
+sudo sysctl --system
+
+#Install containerd
+sudo apt update 
+sudo apt install -y containerd
+
+#Configure containerd
+sudo mkdir -p /etc/containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+
+#Set the cgroup driver for containerd to systemd which is required for the kubelet.
+#For more information on this config file see:
+# https://github.com/containerd/cri/blob/master/docs/config.md and also
+# https://github.com/containerd/containerd/blob/master/docs/ops.md
+sudo sed -i '/SystemdCgroup = true/c\SystemdCgroup = false' /etc/containerd/config.toml
+
+#Restart containerd with the new configuration
+sudo systemctl restart containerd
+
+#Install Kubernetes packages - kubeadm, kubelet and kubectl
+#Add Google's apt repository gpg key
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+#Add the Kubernetes apt repository
+sudo bash -c 'cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF'
+
+#Update the package list 
+sudo apt update
+apt-cache policy kubelet | head -n 20 
+
+#Install the required packages, if needed we can request a specific version. 
+#Pick the same version you used on the Control Plane Node in 0-PackageInstallation-containerd.sh
+VERSION=1.21.0-00
+sudo apt install -y kubelet=$VERSION kubeadm=$VERSION kubectl=$VERSION
+sudo apt-mark hold kubelet kubeadm kubectl containerd
+
+#To install the latest, omit the version parameters
+#sudo apt-get install kubelet kubeadm kubectl
+#sudo apt-mark hold kubelet kubeadm kubectl
+
+#Check the status of our kubelet and our container runtime.
+#The kubelet will enter a crashloop until it's joined
+sudo systemctl status kubelet.service 
+sudo systemctl status containerd.service 
+
+#Ensure both are set to start when the system starts up.
+sudo systemctl enable kubelet.service
+sudo systemctl enable containerd.service
