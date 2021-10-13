cc5e94499ac965407c1b015a0f9373280f912fa7
Julian Edington <julianedington@gmail.com>
2021-10-13 10:06:09 -0500

    bypass prompt for keygen

--- 01-Setup-Ansible/ssh-setup-example.sh
+++ 01-Setup-Ansible/ssh-setup-example.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-# store in controller host '~/<this file>'
-# this is optional
 # example commands to setup ssh--might need to change/add root@'s
 
-ssh-keygen -t rsa -b 4096 -C "Ansible Key"
+# yes '' | ssh-keygen -t rsa -b 4096 -C "Kubenetes Key" -N '' > /dev/null
+# # ^^ Completely no prompts or showing RSA token already made ^^
+
+ssh-keygen -t rsa -b 2048 -C "Kubernetes Key" -N '' # <<<$'\n'
+# ^^ better option, will prompt if it already exists ^^
 ssh-copy-id -i $HOME/.ssh/id_rsa.pub root@c1-cp1
 ssh-copy-id -i $HOME/.ssh/id_rsa.pub root@c1-node1
 ssh-copy-id -i $HOME/.ssh/id_rsa.pub root@c1-node2
 ssh-copy-id -i $HOME/.ssh/id_rsa.pub root@c1-node3
 eval $(ssh-agent)
 ssh-add
-ansible -i ~/etc/hosts -m ping all
-
-#- sudo ansible-playbook -i ~/ all-setup.yml
-# ^ run after setting up ssh/other auth options ^
+ansible -m ping all
