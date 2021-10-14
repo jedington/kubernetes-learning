e82550af3d8f85e6f0ccbb777773d28274643eec
Julian Edington <julianedington@gmail.com>
2021-10-14 00:30:43 -0500

    hosts added in, upgrade, autoclean, autoremove

--- 01-Setup-Ansible/controller-setup.sh
+++ 01-Setup-Ansible/controller-setup.sh
@@ -12,33 +12,61 @@ osInfo[/etc/redhat-release]="yum"
 for f in ${!osInfo[@]}
 do
     if [[ -f $f ]];then
-        package_manager=${osInfo[$f]}
+		pkg_mgr=${osInfo[$f]}
     fi
 done
 
-sudo $package_manager update
-sudo $package_manager upgrade
-if [ $package_manager=="apt" ]
+sudo $pkg_mgr update && sudo $pkg_mgr upgrade
+if [ $pkg_mgr=="apt" ]
 then
     sudo apt install software-properties-common -y
     sudo -E apt-add-repository -y 'ppa:deadsnakes/ppa'
     sudo -E apt-add-repository -y 'ppa:ansible/ansible'
-    sudo apt update
 fi
 
-sudo $package_manager install git -y
-sudo $package_manager install python3 -y
-sudo $package_manager update
-
-#- sudo $package_manager remove ansible && sudo $package_manager --purge autoremove 
+sudo $pkg_mgr update
+sudo $pkg_mgr install git python3 ansible -y
+sudo $pkg_mgr upgrade && sudo $pkg_mgr autoclean && sudo $pkg_mgr autoremove
+ansible --version | grep "python version" # optional
+#- sudo $pkg_mgr remove ansible && sudo $pkg_mgr --purge autoremove 
 # ^ if older/stale ansible version exists ^
-sudo $package_manager install ansible -y
-#- ansible --version | grep "python version" # optional
 
 # ip changes depending on vm setup
-echo "
-10.10.10.10 c1-cp1
-10.10.10.11 c1-node1
-10.10.10.12 c1-node2
-10.10.10.13 c1-node3
-" >> /etc/hosts
+while IFS= read -r line ; do
+    if ! grep -Fqxe "$line" /etc/hosts ; then
+        printf "%s\n" "$line" >> /etc/hosts
+    fi
+done <<- EOM
+
+    ### ###### ###
+    # /etc/hosts #
+    # ## #### ## #
+
+    10.10.10.10 c1-cp1
+    10.10.10.11 c1-node1
+    10.10.10.12 c1-node2
+    10.10.10.13 c1-node3
+
+EOM
+
+# ansible_host=xxx.xxx.xxx.xxx ips change depending on vm setup
+while IFS= read -r line ; do
+    if ! grep -Fqxe "$line" /etc/ansible/hosts ; then
+        printf "%s\n" "$line" >> /etc/ansible/hosts
+    fi
+done <<- EOM
+
+    ### ### ###### ### ###
+    # /etc/ansible/hosts #
+    # ## ### #### ### ## #
+
+    [controller]
+    c1-cp1 ansible_host=10.10.10.10 ansible_user=root
+    #- localhost ansible_host=127.0.0.1 ansible_user=root ansible_connection=local
+
+    [nodes] # ip changes depending on vm setup
+    c1-node1 ansible_host=10.10.10.11 ansible_user=root 
+    c1-node2 ansible_host=10.10.10.12 ansible_user=root 
+    c1-node3 ansible_host=10.10.10.13 ansible_user=root
+
+EOM
